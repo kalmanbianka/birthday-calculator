b606cf3622c8939aa730f591d48e62bcb983e1f0
Kálmán Bianka <kalman.bianka97@gmail.com>
2019-08-04 12:52:32 +0200

    added value to input

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements OnInit {
+  @ViewChild('inputElement', {static: true}) inputElement: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
+  onSubmitBirthYear(){
+    console.log(this.inputElement.nativeElement.value);
+  }
+
 
 }
